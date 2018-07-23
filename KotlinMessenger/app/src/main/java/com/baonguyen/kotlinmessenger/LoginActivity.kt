f6b54ed9ea14be8fd5127d03ff94075789dade98
Bao Nguyen <bao.nguyen@smoovapp.com>
2018-07-23 15:30:21 +0700

    add login view

---
+++ KotlinMessenger/app/src/main/java/com/baonguyen/kotlinmessenger/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.baonguyen.kotlinmessenger
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        buttonLogin.setOnClickListener { _ ->
+
+        }
+    }
+}
---
+++ KotlinMessenger/app/src/main/java/com/baonguyen/kotlinmessenger/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.baonguyen.kotlinmessenger
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonRegister.setOnClickListener { _ ->
+
+        }
+
+        textViewAlreadyAccount.setOnClickListener { _ ->
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
